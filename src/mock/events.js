677a4e30d7bbb48c28fa8c611eea69db9c4834ab
synthetiquely <anatoliybelobrovik@gmail.com>
2019-04-27 12:16:29 +0300

    Add dates to mock data

--- src/mock/events.js
+++ src/mock/events.js
@@ -1,34 +1,54 @@
-export const EVENTS = [
-  {
-    id: "event-1",
-    title: "Рассуждения о высоком",
-    dateStart: new Date(),
-    dateEnd: new Date(),
-    room: "room-1",
-    users: ["user-1", "user-2", "user-3"],
-  },
-  {
-    id: "event-2",
-    title: "Рассуждения о высоком",
-    dateStart: new Date(),
-    dateEnd: new Date(),
-    room: "room-1",
-    users: ["user-1", "user-2", "user-3"],
-  },
-  {
-    id: "event-3",
-    title: "Рассуждения о высоком",
-    dateStart: new Date(),
-    dateEnd: new Date(),
-    room: "room-1",
-    users: ["user-1", "user-2", "user-3"],
-  },
-  {
-    id: "event-4",
-    title: "Рассуждения о высоком",
-    dateStart: new Date(),
-    dateEnd: new Date(),
-    room: "room-1",
-    users: ["user-1", "user-2", "user-3"],
-  },
-];
+export const EVENTS = {
+  "room-1": [
+    {
+      id: "event-1",
+      title: "Рассуждения о высоком",
+      dateStart: new Date("2019-04-27:08:00"),
+      dateEnd: new Date("2019-04-27:09:00"),
+      room: "room-1",
+      users: ["user-1", "user-2", "user-3"],
+    },
+    {
+      id: "event-2",
+      title: "Рассуждения о высоком",
+      dateStart: new Date("2019-04-27:10:00"),
+      dateEnd: new Date("2019-04-27:12:00"),
+      room: "room-1",
+      users: ["user-1", "user-2", "user-3"],
+    },
+    {
+      id: "event-3",
+      title: "Рассуждения о высоком",
+      dateStart: new Date("2019-04-27:14:00"),
+      dateEnd: new Date("2019-04-27:17:00"),
+      room: "room-1",
+      users: ["user-1", "user-2", "user-3"],
+    },
+  ],
+  "room-2": [
+    {
+      id: "event-4",
+      title: "Рассуждения о высоком",
+      dateStart: new Date("2019-04-27:09:00"),
+      dateEnd: new Date("2019-04-27:10:00"),
+      room: "room-2",
+      users: ["user-1", "user-2", "user-3"],
+    },
+    {
+      id: "event-5",
+      title: "Рассуждения о высоком",
+      dateStart: new Date("2019-04-27:10:00"),
+      dateEnd: new Date("2019-04-27:12:00"),
+      room: "room-2",
+      users: ["user-1", "user-2", "user-3"],
+    },
+    {
+      id: "event-6",
+      title: "Рассуждения о высоком",
+      dateStart: new Date("2019-04-27:12:00"),
+      dateEnd: new Date("2019-04-27:18:00"),
+      room: "room-2",
+      users: ["user-1", "user-2", "user-3"],
+    },
+  ],
+};
