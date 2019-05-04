f0bbce188f70c5eccbdb363a210bdd428b41e23d
synthetiquely <anatoliybelobrovik@gmail.com>
2019-05-04 19:04:40 +0300

    Set hours width and timeslots width to be responsive to container's width

--- src/components/Timeslot/Timeslot.jsx
+++ src/components/Timeslot/Timeslot.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
-import { DEFAULT_TIMESLOT_WIDTH } from "../../services/time";
+import { MAX_HOURS } from "../../services/time";
 import Styles from "./Timeslot.module.css";
 
 export function Timeslot({ hourStart, hourEnd, isOccupied }) {
   let diff = 1;
-  let width = DEFAULT_TIMESLOT_WIDTH;
+  let width = 100 / MAX_HOURS;
 
   if (hourStart && hourEnd) {
     diff = hourEnd - hourStart;
     width = width * diff;
   }
-
+ 
   return (
-    <li style={{ width }} className={Styles.container}>
+    <li style={{ width: `${width}%` }} className={Styles.container}>
       <button disabled={isOccupied} className={classnames(Styles.slot, { [Styles["slot--occupied"]]: isOccupied })} />
     </li>
   );
--- src/components/Timeslot/Timeslot.test.jsx
+++ src/components/Timeslot/Timeslot.test.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, cleanup } from "react-testing-library";
 import "jest-dom/extend-expect";
 import { Timeslot } from "./Timeslot";
-import { DEFAULT_TIMESLOT_WIDTH } from "../../services/time";
+import { MAX_HOURS } from "../../services/time";
 
 afterEach(cleanup);
 
@@ -10,16 +10,16 @@ it("renders a Timeslot of a single width", () => {
   const { container } = render(<Timeslot hourStart={8} hourEnd={9} />);
   let component = container.firstChild;
 
-  expect(component.style.width).toEqual(`${DEFAULT_TIMESLOT_WIDTH}px`);
+  expect(component.style.width).toEqual(`${100 / MAX_HOURS}%`);
 });
 
 it("renders a Timeslot of a twice width", () => {
-  let expectedWidth = DEFAULT_TIMESLOT_WIDTH * 2;
+  let expectedWidth = (100 / MAX_HOURS) * 2;
 
   const { container } = render(<Timeslot hourStart={8} hourEnd={10} />);
   let component = container.firstChild;
 
-  expect(component.style.width).toEqual(`${expectedWidth}px`);
+  expect(component.style.width).toEqual(`${expectedWidth}%`);
 });
 
 it("renders a Timeslot with a disabled button if it is occupied", () => {
